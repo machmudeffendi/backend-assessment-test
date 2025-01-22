e39154d2aa22bd6aed2ad292f32a0d56776932e6
Machmud Effendi <machmud.effendi@syngenta.com>
2025-01-23 02:58:54 +0700

    feat: create loan

--- app/Services/LoanService.php
+++ app/Services/LoanService.php
@@ -4,7 +4,10 @@ namespace App\Services;
 
 use App\Models\Loan;
 use App\Models\ReceivedRepayment;
+use App\Models\ScheduledRepayment;
 use App\Models\User;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\DB;
 
 class LoanService
 {
@@ -22,6 +25,37 @@ class LoanService
     public function createLoan(User $user, int $amount, string $currencyCode, int $terms, string $processedAt): Loan
     {
         //
+        return DB::transaction(function () use ($user, $amount, $currencyCode, $terms, $processedAt) {
+            $loan = $user->loans()->create([
+                'amount' => $amount,
+                'outstanding_amount' => $amount,
+                'currency_code' => $currencyCode,
+                'terms' => $terms,
+                'processed_at' => $processedAt,
+                'status' => Loan::STATUS_DUE
+            ]);
+
+            $scheduledRepayments = [];
+            $scheduledRepaymentsAmount = $amount / $terms;
+            $processedDate = Carbon::parse($processedAt);
+            for ($i = 1; $i <= $terms; $i++) {
+                $dueDate = $processedDate->copy()->addMonth($i)->format('Y-m-d');
+                $amountToRepay = ($i === $terms) ? ceil($scheduledRepaymentsAmount) : floor($scheduledRepaymentsAmount);
+
+                array_push($scheduledRepayments, [
+                    'loan_id' => $loan->id,
+                    'amount' => $amountToRepay,
+                    'outstanding_amount' => $amountToRepay,
+                    'currency_code' => $currencyCode,
+                    'due_date' => $dueDate,
+                    'status' => ScheduledRepayment::STATUS_DUE
+                ]);
+            }
+
+            ScheduledRepayment::insert($scheduledRepayments);
+
+            return $loan;
+        });
     }
 
     /**
